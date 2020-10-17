190c4a731ab0bbbff03eca9da473e403201d853b
sky.oh <theneul@gmail.com>
2020-10-18 02:00:02 +0900

    Initial commit

---
+++ AspNetCoreHero.Application/Interfaces/Repositories/IProductRepositoryAsync.cs
@@ -0,0 +1,15 @@
+﻿using AspNetCoreHero.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AspNetCoreHero.Application.Interfaces.Repositories
+{
+    public interface IProductRepositoryAsync : IGenericRepositoryAsync<Product>
+    {
+        Task<IReadOnlyList<Product>> GetAllWithCategoriesAsync(bool isCached = false);
+        Task<IReadOnlyList<Product>> GetAllWithCategoriesWithoutImagesAsync(bool isCached = false);
+        Task<bool> IsUniqueBarcodeAsync(string barcode);
+    }
+}
